96a3fec0c2f15361596330237cc4b172b2c64a7d
William McGonagle <wpmcgonagle@gmail.com>
2022-02-05 21:53:48 -0500

    🎉 Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+function getName() {
+
+    return 'JavaScript';
+
+}
+
+function getStatus() {
+
+    // Needs Work
+    // In-Development
+    // Working
+    // Done
+    // Deprecated
+
+    return 1;
+
+}
+
+module.exports = {
+    getName,
+    getStatus
+};
